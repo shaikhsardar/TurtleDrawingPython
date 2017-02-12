f2ba08c3d0d06d11c4a9e4eff04b47a73d6c6eed
Shaikh Sardar Salim <shaikhsardar@users.noreply.github.com>
2017-02-12 22:10:38 +0530

    Create Sunflower.py

---
+++ Sunflower.py
@@ -0,0 +1,32 @@
+import turtle
+
+def drawFlower():
+    
+    window = turtle.Screen()
+    window.bgcolor("blue")
+    petals = 36
+    rhombus = 45
+    size = 60
+    draw = turtle.Turtle()
+    draw.shape("circle")
+    draw.color("green")
+    draw.speed(0)
+    draw.setheading(270)
+   
+    for i in range(0,petals):
+        oblique = True
+        for x in range(0,4):
+            draw.forward(size)
+            if(oblique):
+                draw.right(90-rhombus)
+                oblique = False
+            else:
+                draw.right(90+rhombus)
+                oblique = True
+            draw.right(360/petals)
+        draw.right(360/petals)
+    draw.forward(size*5)
+    
+    window.exitonclick()
+
+drawFlower()
